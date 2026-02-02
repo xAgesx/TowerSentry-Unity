138b1a4b487a2fa02e77989017fecbb2d8b8c3dc
xAgesx <thamer.douss@gmail.com>
2026-02-02 21:49:53 +0100

    added preview for unit selection

---
+++ Assets/TroopSpawn.cs
@@ -0,0 +1,47 @@
+using Unity.Mathematics;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class TroopSpawn : MonoBehaviour {
+    
+    [Header("Prefabs&Materials")]
+    public Material previewMaterial ;
+    public Material errorPreviewMaterial;
+    public GameObject previewPrefab;
+
+    [Header("Properties")]
+    public float range = 15f;
+    public bool isPreview = false;
+    GameObject previewObj;
+    EntitiesManager entitiesManager;
+    void Start() {
+        entitiesManager = GetComponent<EntitiesManager>();
+    }
+    void Update() {
+        if(previewObj == null)return;
+
+
+        if (Mouse.current.leftButton.isPressed) {
+            Debug.Log("Clicked");
+            if (getDistance(previewObj.transform.position, Vector3.zero) <= range) {
+            Instantiate(entitiesManager.troopPrefabs[0],previewObj.transform);
+        }
+        }
+    }
+    public void PreviewUnit() {
+        isPreview = !isPreview;
+        if (isPreview) {
+            Vector3 mousePos = Mouse.current.position.ReadValue();
+            Vector3 worldPos = Camera.main.ScreenToWorldPoint(mousePos);
+            previewObj = Instantiate(previewPrefab,worldPos+new Vector3(0,-35,0),quaternion.identity);
+            previewObj.GetComponent<Renderer>().material = previewMaterial;
+        } 
+        
+    }
+    public float getDistance(Vector3 pos1 , Vector3 pos2) {
+        return Vector3.Distance(pos1,pos2);
+        
+    }
+    
+}
