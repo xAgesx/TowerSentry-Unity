526c0cd4f361b97d9dcf4c98343b98d60d51e491
xAgesx <thamer.douss@gmail.com>
2026-02-03 21:35:18 +0100

    Added Troop Spawning Logic + troop preview

--- Assets/TroopSpawn.cs
+++ Assets/TroopSpawn.cs
@@ -1,3 +1,4 @@
+using System;
 using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -14,29 +15,33 @@ public class TroopSpawn : MonoBehaviour {
     public bool isPreview = false;
     GameObject previewObj;
     EntitiesManager entitiesManager;
+    public int selectedTroopIndex = -1;
     void Start() {
         entitiesManager = GetComponent<EntitiesManager>();
     }
     void Update() {
         if (previewObj == null) return;
-
+        if(selectedTroopIndex == -1) return;
 
         if (Mouse.current.leftButton.wasPressedThisFrame) {
             Debug.Log("Spawned Troop");
             if (getDistance(previewObj.transform.position, Vector3.zero) <= range) {
-                Instantiate(entitiesManager.troopPrefabs[0], previewObj.transform.position, quaternion.identity);
+                Instantiate(entitiesManager.troopPrefabs[selectedTroopIndex], previewObj.transform.position, quaternion.identity);
             }
         } else if (Mouse.current.rightButton.wasPressedThisFrame) {
             isPreview = false;
+            selectedTroopIndex = -1;
         }
     }
-    public void PreviewUnit() {
-        isPreview = !isPreview;
+    public void PreviewUnit(int index) {
+        if(selectedTroopIndex == index || selectedTroopIndex == -1)  isPreview = !isPreview;
+        selectedTroopIndex = index;
         if (isPreview) {
             Vector3 mousePos = Mouse.current.position.ReadValue();
             Vector3 worldPos = Camera.main.ScreenToWorldPoint(mousePos);
             previewObj = Instantiate(previewPrefab, worldPos + new Vector3(0, -35, 0), quaternion.identity);
             previewObj.GetComponent<Renderer>().material = previewMaterial;
+            previewObj.GetComponent<MeshFilter>().sharedMesh = entitiesManager.troopPrefabs[selectedTroopIndex].GetComponent<MeshFilter>().sharedMesh;
         }
 
     }
@@ -44,5 +49,6 @@ public class TroopSpawn : MonoBehaviour {
         return Vector3.Distance(pos1, pos2);
 
     }
+    
 
 }
