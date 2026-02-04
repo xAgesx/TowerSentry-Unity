9f8c9c5c585d54734fb04545f7dca909c4f1a6c0
xAgesx <thamer.douss@gmail.com>
2026-02-04 20:56:10 +0100

    Added Basic GameLoop

--- Assets/Scripts/EnemyHealth.cs
+++ Assets/Scripts/EnemyHealth.cs
@@ -21,6 +21,7 @@ public class EnemyHealth : MonoBehaviour {
 
     void Die() {
         Debug.Log(gameObject.name + " died");
+        FindAnyObjectByType<GameManager>().EnemyDied();
         em.enemies.Remove(gameObject);
         Destroy(gameObject);
     }
