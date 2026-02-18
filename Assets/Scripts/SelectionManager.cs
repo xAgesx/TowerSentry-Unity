90fb1b37cd61f4f7f96a7a61608485682db6f4eb
xAgesx <thamer.douss@gmail.com>
2026-02-18 22:07:59 +0100

    Stats panel now keep being displayed when clicking on the entity

--- Assets/Scripts/SelectionManager.cs
+++ Assets/Scripts/SelectionManager.cs
@@ -4,6 +4,8 @@ using UnityEngine.InputSystem;
 public class SelectionManager : MonoBehaviour {
     private HoverDisplay currentHover;
     private Camera mainCam;
+    public bool isVisible = false;
+    private HoverDisplay selectedUnit;
 
     void Start() {
         mainCam = Camera.main;
@@ -16,28 +18,45 @@ public class SelectionManager : MonoBehaviour {
     void FollowMouseWithRay() {
         Vector2 mousePos = Mouse.current.position.ReadValue();
         Ray ray = mainCam.ScreenPointToRay(mousePos);
+        bool hitSomething = Physics.Raycast(ray, out RaycastHit hit);
 
-        if (Physics.Raycast(ray, out RaycastHit hit)) {
-            HoverDisplay unit = hit.collider.GetComponent<HoverDisplay>();
-
-            if (unit != currentHover) {
-                if (currentHover != null) {
-                    currentHover.SetOverlayVisible(false);
-                    currentHover.SetStatsVisible(false);
-                }
-                currentHover = unit;
-                if (currentHover != null) {
-                    currentHover.SetOverlayVisible(true);
-                    currentHover.SetStatsVisible(true);
-
-                }
-            }
-        } else {
-            if (currentHover != null) {
+        HoverDisplay unitUnderMouse = hitSomething ? hit.collider.GetComponent<HoverDisplay>() : null;
+        if (unitUnderMouse != currentHover) {
+            // Hide old hover UI ONLY if it's not the selected unit
+            if (currentHover != null && currentHover != selectedUnit) {
                 currentHover.SetOverlayVisible(false);
                 currentHover.SetStatsVisible(false);
+            }
 
-                currentHover = null;
+            currentHover = unitUnderMouse;
+
+            // Show new hover UI
+            if (currentHover != null) {
+                currentHover.SetOverlayVisible(true);
+                currentHover.SetStatsVisible(true);
+            }
+        }
+
+        
+        if (Mouse.current.leftButton.wasPressedThisFrame) {
+            // If clicked a unit, select it
+            if (unitUnderMouse != null) {
+                // Deselect old unit UI if it's not the one currently the player is hovering
+                if (selectedUnit != null && selectedUnit != unitUnderMouse) {
+                    selectedUnit.SetOverlayVisible(false);
+                    selectedUnit.SetStatsVisible(false);
+                }
+                selectedUnit = unitUnderMouse;
+                Debug.Log("Unit Selected: " + selectedUnit.name);
+            }
+            // If clicked empty space, deselect everything
+            else {
+                if (selectedUnit != null) {
+                    selectedUnit.SetOverlayVisible(false);
+                    selectedUnit.SetStatsVisible(false);
+                    selectedUnit = null;
+                    Debug.Log("Deselected everything");
+                }
             }
         }
     }
